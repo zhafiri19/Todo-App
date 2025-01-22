ee61881c8599f786aa12047f5c27ddfa9f65d3d4
zhafiri19 <zhafirifarhan5@gmail.com>
2025-01-23 00:30:30 +0700

    penambahan todo

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
+<?php
+
+$todos = [];
+$file = file_get_contents('todo.txt');
+$todos = unserialize($file);
+
+if (isset($_POST['todo'])) {
+    $data = $_POST['todo'];
+    $todos[] = [
+        'todo' => $data,
+        'status' => 0
+    ];
+    file_put_contents('todo.txt', serialize($todos));
+}
+?>
+
 <h1>Todo App</h1>
 
-<form action="" method="post">
+<form method="post">
     <label for="">List Tontonan Anime</label>
     <input type="text" name="todo">
     <button type="submit">Simpan</button>
@@ -8,7 +24,7 @@
 
 <ul>
     <li>
-        <input type="checkbox" name="todo[]">
+        <input type="checkbox" name="todo">
         <label>Todo 1</label>
         <a href="#">Hapus</a>
     </li>
