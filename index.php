a4a17bd36d136c077f94288d73c6bee9952fb8a3
zhafiri19 <zhafirifarhan5@gmail.com>
2025-01-23 01:44:37 +0700

    Todo App Anime

--- index.php
+++ index.php
@@ -1,16 +1,37 @@
 <?php
+$todos = []; // total array yang disiapkan untuk disimpan
+//jika file todo.txt untuk membaca data, bila belum ada biarkan saja
+if (file_exists('todo.txt')) {
+    $file = file_get_contents('todo.txt'); // membaca file todo
+    $todos = unserialize($file); // mengubah format serialize menjadi array
+}
 
-$todos = [];
-$file = file_get_contents('todo.txt');
-$todos = unserialize($file);
-
-if (isset($_POST['todo'])) {
+//jika ditemukan todo yang dikirimkan melalui method post
+if (isset($_POST['todo'])) { //mengambil data yang di input pada form
     $data = $_POST['todo'];
     $todos[] = [
         'todo' => $data,
         'status' => 0
     ];
+    simpanAnime($todos);
+}
+
+if (isset($_GET['status'])) {
+    $todos[$_GET['key']]['status'] = $_GET['status'];
+    simpanAnime($todos);
+}
+
+//hapus list todo
+if (isset($_GET['hapus'])) {
+    unset($todos[$_GET['key']]);
+    simpanAnime($todos);
+}
+
+//membuat fungsi
+function simpanAnime($todos)
+{
     file_put_contents('todo.txt', serialize($todos));
+    header('Location:index.php');
 }
 ?>
 
@@ -23,9 +44,20 @@ if (isset($_POST['todo'])) {
 </form>
 
 <ul>
-    <li>
-        <input type="checkbox" name="todo">
-        <label>Todo 1</label>
-        <a href="#">Hapus</a>
-    </li>
+    <?php
+    foreach ($todos as $key => $value):  ?>
+        <li>
+            <input type="checkbox" name="todo" onclick="window.location.href = 'index.php?status=<?php echo ($value['status'] == 1) ? '0' :
+                                                                                                        '1'; ?> &key=<?php echo $key; ?>'" <?php if ($value['status'] == 1) echo 'checked' ?>>
+            <label>
+                <?php
+                if ($value['status'] == 1)
+                    echo '<del>' . $value['todo'] . '</del>';
+                else
+                    echo $value['todo'];
+                ?>
+            </label>
+            <a href="index.php?hapus=1&key=<?php echo $key; ?>" onclick="return confirm('Apakah anda yakin ingin menghapus list anime <?php echo $value['todo']; ?> ?' )">Hapus</a>
+        </li>
+    <?php endforeach; ?>
 </ul>
