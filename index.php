adcee1f101287a48b691c4462a7144a86fd85041
zhafiri19 <zhafirifarhan5@gmail.com>
2025-01-23 00:03:17 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<h1>Todo App</h1>
+
+<form action="" method="post">
+    <label for="">List Tontonan Anime</label>
+    <input type="text" name="todo">
+    <button type="submit">Simpan</button>
+</form>
+
+<ul>
+    <li>
+        <input type="checkbox" name="todo[]">
+        <label>Todo 1</label>
+        <a href="#">Hapus</a>
+    </li>
+</ul>
